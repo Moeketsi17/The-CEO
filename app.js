e3eb799f5b5bf9a7845e7954117fe52ca8a2cca4
raymond <raymondkxng17@gmail.com>
2024-08-11 21:19:34 +0200

    header

---
+++ app.js
@@ -0,0 +1,19 @@
+const burger =  document.querySelector(".burger");
+const navlinks = document.querySelector(".navlinks-container")
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active")
+    navlinks.classList.toggle("active")
+
+})
+
+
+
+
+
+
+
+
+
+
+
